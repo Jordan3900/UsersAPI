e78b430fd5c0025805d7edf36fcc2ffd5ff5763c
Yordan <pm.yordan@gmail.com>
2020-04-16 19:07:35 +0300

    Initial create

---
+++ src/UsersAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using UsersAPI.Models;
+
+namespace UsersAPI.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<User>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
